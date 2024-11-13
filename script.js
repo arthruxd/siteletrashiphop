b35facb91aa09f25e49306fd99e0b46dc3235b71
arthruxd <81787374+arthruxd@users.noreply.github.com>
2024-11-13 09:00:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,22 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const redirectButton = document.getElementById('redirectButton');
+      function createParticles() {
+        const container = document.createElement('div');
+        container.classList.add('background-particles');
+        document.body.appendChild(container);
 
-    // Adiciona evento de clique para redirecionar para a página de letras
-    redirectButton.addEventListener('click', () => {
-        window.location.href = 'letras/letrashiphop.html';
-    });
-
-    // Efeito de suavização ao carregar a página
-    document.body.style.opacity = 0;
-    document.body.style.transition = 'opacity 1s';
-    setTimeout(() => {
-        document.body.style.opacity = 1;
-    }, 100);
-});
+        for (let i = 0; i < 50; i++) {
+            const particle = document.createElement('div');
+            particle.classList.add('particle');
+            
+            particle.style.width = `${Math.random() * 5 + 2}px`;
+            particle.style.height = particle.style.width;
+            particle.style.left = `${Math.random() * 100}%`;
+            particle.style.top = `${Math.random() * 100}%`;
+            particle.style.animationDuration = `${Math.random() * 10 + 5}s`;
+            
+            container.appendChild(particle);
+        }
+    }
+    createParticles();
