e3a312b7e69f172118dafc000c5366182ea6a9ce
arthruxd <81787374+arthruxd@users.noreply.github.com>
2024-11-13 08:51:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// Redireciona para a página de letras de hip-hop
-document.getElementById('redirectButton').addEventListener('click', function() {
-    window.location.href = 'letras/letrashiphop.html'; // Caminho relativo para a página de letras
+// Script para funcionalidade da página inicial do Projeto Letras de Hip Hop
+
+document.addEventListener('DOMContentLoaded', () => {
+    const redirectButton = document.getElementById('redirectButton');
+
+    // Adiciona evento de clique para redirecionar para a página de letras
+    redirectButton.addEventListener('click', () => {
+        window.location.href = 'letras/letrashiphop.html';
+    });
+
+    // Efeito de suavização ao carregar a página
+    document.body.style.opacity = 0;
+    document.body.style.transition = 'opacity 1s';
+    setTimeout(() => {
+        document.body.style.opacity = 1;
+    }, 100);
 });
