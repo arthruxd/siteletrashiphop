ea23754914414c1abfae9caf6a69c1a1aa2437a0
arthruxd <81787374+arthruxd@users.noreply.github.com>
2024-11-13 08:28:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// Redireciona para a página de envio de letras ao clicar no botão
+document.getElementById('redirectButton').addEventListener('click', function() {
+    window.location.href = '/letras/letrashiphop.html';
+});
 
